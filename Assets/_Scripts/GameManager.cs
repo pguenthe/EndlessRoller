6d841e10bbedcc00a4dbc3ed566f2234f00cb80f
Peter Guenther <peter.guenther@gmail.com>
2024-12-04 10:04:22 -0500

    removed platform gen code for class demo

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,19 +6,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject PlatformParent;
-    public GameObject[] PlatformPrefabs;
+
     public TextMeshProUGUI GameStatusText;
     public TextMeshProUGUI DistanceText;
     public GameObject PlayAgainButton;
-    public float PlatformLength = 6;
-    public int StartingPlatforms = 4;
 
-    public static float Speed = 7.5f;
+
+
     public static bool Running;
 
     private float DistanceTraveled;
-    private List<GameObject> Platforms;
+
 
     void Start()
     {
@@ -27,18 +25,7 @@ public class GameManager : MonoBehaviour
         GameStatusText.gameObject.SetActive(false);
         PlayAgainButton.gameObject.SetActive(false);
 
-        Platforms = new List<GameObject>();
 
-        GameObject obj = Instantiate(PlatformPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity, PlatformParent.transform);
-        Platforms.Add(obj);
-
-        for (int i = 1; i <= StartingPlatforms; i++)
-        {
-            int n = UnityEngine.Random.Range(0, PlatformPrefabs.Length - 1);
-            obj = Instantiate(PlatformPrefabs[n], new Vector3(0, 0, i * PlatformLength), Quaternion.identity, PlatformParent.transform);
-            Debug.Log($"Generated platform {n} at z={obj.transform.position.z}");
-            Platforms.Add(obj);
-        }
     }
 
     // Update is called once per frame
@@ -52,20 +39,6 @@ public class GameManager : MonoBehaviour
             PlayAgainButton.gameObject.SetActive(true);
         }
 
-        if (Running)
-        {
-            DistanceTraveled += Time.deltaTime * Speed;
-            DistanceText.text = DistanceTraveled.ToString("0000.00");
-
-            if (Platforms[0].transform.position.z <= -1 * PlatformLength)
-            {
-                Destroy(Platforms[0]);
-                Platforms.RemoveAt(0);
-
-                int n = UnityEngine.Random.Range(0, PlatformPrefabs.Length - 1);
-                float z = Platforms[Platforms.Count - 1].transform.position.z + PlatformLength;
-                Platforms.Add(Instantiate(PlatformPrefabs[n], new Vector3(0, 0, z), Quaternion.identity, PlatformParent.transform));
-            }
-        }
+        
     }
 }
