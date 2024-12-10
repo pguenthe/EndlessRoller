e60bbb900040985be8354cc7830b523b48d31ce1
Peter Guenther <peter.guenther@gmail.com>
2024-12-09 21:52:53 -0500

    added progression of platform difficulty

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -19,6 +19,11 @@ public class GameManager : MonoBehaviour
     public float PlatformOverlap = 0.1f;
     public int ShiftXEveryCount = 7;
     public int ShiftYEveryCount = 12;
+    public int EasyPlatformPrefabCount;
+    public int MediumPlatformPrefabCount;
+    public int PlatformCountBeforeMedium = 10;
+    public int PlatformCountBeforeHard = 18;
+    public int FullPlatformsToStart = 2;
 
     public static float Speed = 7.8f; //2.2f;
     public static bool Running;
@@ -43,12 +48,18 @@ public class GameManager : MonoBehaviour
         PlayAgainButton.gameObject.SetActive(false);
 
         Platforms = new List<GameObject>();
-        GameObject obj = Instantiate(PlatformPrefabs[0], Vector3.zero, Quaternion.identity, PlatformParent.transform);
-        Platforms.Add(obj);
+        GameObject obj;
 
-        for (int i = 1; i < StartingPlatformCount; i++)
+        for (int i = 0; i < FullPlatformsToStart; i++)
         {
-            int n = UnityEngine.Random.Range(0, Platforms.Count);
+            obj = Instantiate(PlatformPrefabs[0], new Vector3(0, 0, i * PlatformLength - PlatformOverlap), Quaternion.identity, PlatformParent.transform);
+            Platforms.Add(obj);
+            PlatformCount++;
+        }
+
+        for (int i = FullPlatformsToStart; i < StartingPlatformCount; i++)
+        {
+            int n = UnityEngine.Random.Range(0, EasyPlatformPrefabCount);
             obj = Instantiate(PlatformPrefabs[n], new Vector3(0, 0, i * PlatformLength - PlatformOverlap), Quaternion.identity, PlatformParent.transform);
             Platforms.Add(obj);
             PlatformCount++;
@@ -82,51 +93,66 @@ public class GameManager : MonoBehaviour
             //check position of first platform (0)
             if (Platforms[0].transform.position.z < Player.transform.position.z - .7 * PlatformLength)
             {
-                //if it's far enough back
-                //delete it from the scene and remove it from the list
-                Destroy(Platforms[0]);
-                Platforms.RemoveAt(0);
-
-                //shift left or right every so often
-                if (PlatformCount % ShiftXEveryCount == 0)
-                {
-                    if (UnityEngine.Random.value <= .5f)
-                    {
-                        PlatformX += 1;
-                    }
-                    else
-                    {
-                        PlatformX -= 1;
-                    }
-                }
-
-                int n = UnityEngine.Random.Range(0, PlatformPrefabs.Length);
-                float zOffset = 0;
-
-                if (PlatformCount % ShiftYEveryCount == 0)
-                {
-                    n = 0;
-                    if (UnityEngine.Random.value <= .5f)
-                    {
-                        PlatformY += 1;
-                        zOffset = 0.7f;
-                    }
-                    else
-                    {
-                        PlatformY -= 1;
-                        zOffset = 0.4f;
-                    }
-                }
-
-                //add a new one to the end
-                //figure out the position of the last one and add one length
-                float z = Platforms[Platforms.Count - 1].transform.position.z + PlatformLength - PlatformOverlap;
-                GameObject obj = Instantiate(PlatformPrefabs[n], new Vector3(PlatformX, PlatformY, z + zOffset), Quaternion.identity, PlatformParent.transform);
-                Platforms.Add(obj);
-                PlatformCount++;
+                GenerateNewPlatform();
             }
         }
 
+    }
+
+    private void GenerateNewPlatform()
+    {
+        //if it's far enough back
+        //delete it from the scene and remove it from the list
+        Destroy(Platforms[0]);
+        Platforms.RemoveAt(0);
+
+        //shift left or right every so often
+        if (PlatformCount % ShiftXEveryCount == 0)
+        {
+            if (UnityEngine.Random.value <= .5f)
+            {
+                PlatformX += 1;
+            }
+            else
+            {
+                PlatformX -= 1;
+            }
+        }
+
+        int rangeMax = PlatformPrefabs.Length;
+        if (PlatformCount < PlatformCountBeforeMedium)
+        {
+            rangeMax = EasyPlatformPrefabCount;
+        }
+        else if (PlatformCount < PlatformCountBeforeHard)
+        {
+            rangeMax = EasyPlatformPrefabCount + MediumPlatformPrefabCount;
+        }
+
+        int n = UnityEngine.Random.Range(0, rangeMax);
+        float zOffset = 0;
+
+        if (PlatformCount % ShiftYEveryCount == 0)
+        {
+            n = 0;
+            if (UnityEngine.Random.value <= .5f)
+            {
+                PlatformY += 1;
+                zOffset = 0.7f;
+            }
+            else
+            {
+                PlatformY -= 1;
+                zOffset = 0.4f;
+            }
+        }
+
+        //add a new one to the end
+        //figure out the position of the last one and add one length
+        float z = Platforms[Platforms.Count - 1].transform.position.z + PlatformLength - PlatformOverlap;
+        GameObject obj = Instantiate(PlatformPrefabs[n], new Vector3(PlatformX, PlatformY, z + zOffset), Quaternion.identity, PlatformParent.transform);
+        Platforms.Add(obj);
+        PlatformCount++;
     }
 
     private void ShowScore()
